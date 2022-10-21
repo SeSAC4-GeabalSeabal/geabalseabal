f9aec42ceac2b68a800ffcd59f5de336da07e758
choilettuce <tkdgnstkdcn@naver.com>
2022-10-21 17:05:04 +0900

    feat: facechat 기능 추가

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import { Nav } from './components';
 import './App.css';
 import Login from './components/login/Login';
+import FaceChat from './components/FaceChat/FaceChat';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <div>
       <Routes>
         <Route path="/login" exact element={<Login />}></Route>
+        <Route path="/FaceChat" exact element={<FaceChat />}></Route>
       </Routes>
       </div>
     </div>
