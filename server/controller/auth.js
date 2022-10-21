3904170f9fb5345b642498ba7b444d8cf4527bcf
kimyeaeun96 <mangokim96@gmail.com>
2022-10-21 18:47:08 +0900

    feat: Create Kakao login

--- server/controller/auth.js
+++ server/controller/auth.js
@@ -1,27 +1,41 @@
 /* 메인 페이지 */
 exports.getMain = (req, res) => {
-  console.log( req.session ); // 패스포트 세션
-  console.log( req.user ); // 요렇게 써도 사용가능
-  res.send('메롱');
+  console.log(req.session); // 패스포트 세션
+  console.log(req.user); // 요렇게 써도 사용가능
+  res.send("메롱");
 };
 
 /* 로그인 페이지 */
 exports.getLogin = (req, res) => {
-  console.log( req.session ); // 패스포트 세션
-  console.log( req.user ); 
-  res.render('login');
+  console.log(req.session); // 패스포트 세션
+  console.log(req.user);
+  res.render("login");
 };
 
 /* passport */
-const passport = require('../passport/index.js'); 
+const passport = require("../passport/index.js");
 
 /* 패스포트 로그인 기능 */
-exports.getGoogle =  passport.authenticate('google', { scope: ['email', 'profile']});
-exports.getCallback = 
-  passport.authenticate('google', {
-    successRedirect: 'http://localhost:3000', 
-    failureRedirect: 'http://localhost:8000/login', // 로그인 실패시
-  }), 
+/* 구글 로그인 */
+exports.getGoogle = passport.authenticate("google", {
+  scope: ["email", "profile"],
+});
+(exports.getCallback = passport.authenticate("google", {
+  successRedirect: "http://localhost:3000",
+  failureRedirect: "http://localhost:8000/login", // 로그인 실패시
+})),
   (req, res) => {
-    res.redirect('/')
-  }
+    res.redirect("/");
+  };
+
+/* 카카오 로그인 */
+exports.getKakao = passport.authenticate("kakao", {
+  scope: ["profile_nickname", "account_email"],
+});
+(exports.getKakaoCallback = passport.authenticate("kakao", {
+  successRedirect: "http://localhost:3000",
+  failureRedirect: "http://localhost:8000/login", // 로그인 실패시
+})),
+  (req, res) => {
+    res.redirect("/");
+  };
--- server/model/User.js
+++ server/model/User.js
@@ -1,37 +1,37 @@
 const User = (Sequelize, DataTypes) => {
   const model = Sequelize.define(
     // 모델 이름
-    'user',
+    "user",
     // 컬럼 정의
     {
       user_id: {
-        type: DataTypes.STRING(45), 
-        allowNull: false, 
-        primaryKey: true, 
+        type: DataTypes.STRING(255),
+        allowNull: false,
+        primaryKey: true,
       },
       user_name: {
-        type: DataTypes.STRING(20),
-        allowNull: true, 
+        type: DataTypes.STRING(255),
+        allowNull: true,
       },
       user_email: {
-        type: DataTypes.STRING(45),
-        allowNull: false, 
+        type: DataTypes.STRING(255),
+        allowNull: false,
       },
       user_image: {
         type: DataTypes.STRING(100),
-        defaultValue: 'user_default.jpg',
+        defaultValue: "user_default.jpg",
       },
     },
     // 모델의 옵션
     {
       charset: "utf8", // 한국어 설정
       collate: "utf8_general_ci", // 한국어 설정
-      timestamps: false, 
-      tableName: 'user', 
+      timestamps: false,
+      tableName: "user",
       freezeTableName: true,
     }
   );
   return model;
-}
+};
 
 module.exports = User;
