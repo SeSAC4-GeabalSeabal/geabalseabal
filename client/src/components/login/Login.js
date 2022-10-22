e60363fbf2e4728af2f8054d7f3583d3381f72a7
choilettuce <tkdgnstkdcn@naver.com>
2022-10-22 22:00:29 +0900

    feat: Design login page

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -1,12 +1,23 @@
-
 import './Login.scss';
 import axios from 'axios';
 
 const Login = () => {
   return(
+    
     <div className="Login">
-      <a href="http://localhost:8000/login/google">go to googlePassport</a>
+      <h2>Social Login Button</h2>
+      <a href="http://localhost:8000/login/google" className="Loginbox" id="google-connect"> <span>Connect with Google</span></a>
+      <a href="http://localhost:8000/login/kakao" className="Loginbox" id="kakao-connect"> <span>Connect with kakao</span></a>
     </div>
+    
   );
 }
 export default Login;
+
+
+<div class="login-box">
+      
+      <a href="#" class="social-button" id="facebook-connect"> <span>Connect with Facebook</span></a>
+      
+      <a href="#" class="social-button" > <span>Connect with LinkedIn</span></a>
+    </div>
