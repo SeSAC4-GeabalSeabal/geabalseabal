8d6a29a82a1f1c634266f608a6fbd734b2577aa8
sihagwak <sasha.gwak@gmail.com>
2022-10-23 01:06:20 +0900

    feat: room name 넘겨주기 ref 성공  및 websocket 연결 중

--- client/src/components/Room/Room.js
+++ client/src/components/Room/Room.js
@@ -1,20 +1,34 @@
 import axios from 'axios';
+import io from 'socket.io-client';
+import React, { useEffec, useRef } from 'react';
+
+export const socket = io('http://localhost:8000')
+export const SocketContext = React.createContext();
 
 const Room = () => {
-    const sendRoom = async () => {
-        await axios({
-            method: 'post',
-            url: '/room',
-            data: {
-                name: ''
-            }
-        });
-        // name 가져오는 거는 아래 input 창 만들고 ref로 연결해서 가져오기
-        // 코딩온 플랫폼의 DOM에 접근하는 ref 수업 듣고 만들어라. 
-    }
+  const inputRef = useRef(null);
+  // const sendRoom = async () => {
+    // await axios({
+    //   method: 'post',
+    //   url: '/room',
+    //   data: {
+    //       name: ''
+    //   }
+    // });
+    // name 가져오는 거는 아래 input 창 만들고 ref로 연결해서 가져오기
+    // 코딩온 플랫폼의 DOM에 접근하는 ref 수업 듣고 만들어라. 
+  // }
+  function event() {
+    const roomName = inputRef.current.value;
+    console.log(inputRef.current.value);
+    inputRef.current.value = '';
+    socket.emit('join_room', roomName);
+    console.log('시하 바보');
+  };
   return(
     <div style={{marginTop: "200px"}}>
-        adfasdf
+      <input type='test' placeholder='방 이름' name='roomName' ref={inputRef}></input>
+      <button onClick={ event }>전송</button>
     </div>
   );
 }
