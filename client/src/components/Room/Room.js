6d55c04d0f43d6caa778d69a379a139fb787c144
kimyeaeun96 <mangokim96@gmail.com>
2022-10-24 17:02:03 +0900

    feat: receive offer  진행중

--- client/src/components/Room/Room.js
+++ client/src/components/Room/Room.js
@@ -1,10 +1,11 @@
-import axios from 'axios';
-import io from 'socket.io-client';
-import Chat from '../Chat/Chat'
-import React, { useEffect, useRef, useState } from 'react';
-import GetWebcam from '../getWebCam/GetWebCam';
+import axios from "axios";
+import io from "socket.io-client";
+import Chat from "../Chat/Chat";
+import React, { useEffect, useRef, useState } from "react";
+import GetWebcam from "../getWebCam/GetWebCam";
 
 const socket = io.connect("http://localhost:8000");
+let myPeerConnection = new RTCPeerConnection();
 
 const Room = () => {
   const [playing, setPlaying] = useState(null);
@@ -15,14 +16,28 @@ const Room = () => {
   function event() {
     const roomName = inputRef.current.value;
     console.log(inputRef.current.value);
-    inputRef.current.value = '';
-    socket.emit('join_room', roomName);
-    console.log('룸 생성 성공');
+    inputRef.current.value = "";
+    socket.emit("join_room", roomName);
+    console.log("룸 생성 성공");
     GetWebcam((stream) => {
       setPlaying(true);
       videoRef.current.srcObject = stream;
-    }); 
-  };
+    });
+    // Peer A : make offer
+    const offer = myPeerConnection.createOffer();
+    myPeerConnection.setLocalDescription(offer);
+    console.log("Sent the offer");
+    socket.emit("offer", offer, roomName);
+    // Peer B : answer
+    socket.on("offer", (offer) => {
+      myPeerConnection.setRemoteDescription(offer);
+      const answer = myPeerConnection.createAnswer();
+      myPeerConnection.setLocalDescription(answer);
+      // socket.emit("answer", answer, roomName);
+      // console.log("sent the answer");
+    });
+  }
+
   const startOrStop = () => {
     if (playing) {
       const s = videoRef.current.srcObject;
@@ -30,30 +45,36 @@ const Room = () => {
         track.stop();
       });
     } else {
-      GetWebcam((stream => {
+      GetWebcam((stream) => {
         setPlaying(true);
         videoRef.current.srcObject = stream;
-      }));
+      });
     }
     setPlaying(!playing);
-  }
-  return(
+  };
+  return (
     // 방 input
     <>
-    <div className='RoomApp'>
-    <div className="roomData" style={{marginTop: "200px"}}>
-      <input type='test' placeholder='방 이름' name='roomName' ref={inputRef}></input>
-      <button onClick={ event }>전송</button>
-    </div>
-    <Chat />
-    </div>
+      <div className="RoomApp">
+        <div className="roomData" style={{ marginTop: "200px" }}>
+          <input
+            type="test"
+            placeholder="방 이름"
+            name="roomName"
+            ref={inputRef}
+          ></input>
+          <button onClick={event}>전송</button>
+        </div>
+        <Chat />
+      </div>
       <div>
-        <video ref={videoRef} autoPlay /> 
-        <button onClick={() => startOrStop()}>{playing? 'Stop':'Start'}</button>
+        <video ref={videoRef} autoPlay />
+        <button onClick={() => startOrStop()}>
+          {playing ? "Stop" : "Start"}
+        </button>
       </div>
     </>
-    // 방제목 입력 되면서 방 join되는 부분까지 확인하였고, 방 join되면서 히든 처리해주고 화면공유 부분이랑 채팅 태그로 여기다 추가해주세요~~!! 
+    // 방제목 입력 되면서 방 join되는 부분까지 확인하였고, 방 join되면서 히든 처리해주고 화면공유 부분이랑 채팅 태그로 여기다 추가해주세요~~!!
   );
-}
+};
 export default Room;
-
