4227163d081f2f21fc6b77a1ecc0febf0e81a12a
choilettuce <tkdgnstkdcn@naver.com>
2022-10-28 13:45:32 +0900

    docs : webgetscreen

--- client/src/components/getWebScreen/GetWebScreen.js
+++ client/src/components/getWebScreen/GetWebScreen.js
@@ -15,3 +15,28 @@ const GetWebScreen = async () => {
 }
 
 export default GetWebScreen;
+
+// const GetWebScreen = () => {
+//   navigator.mediaDevices
+//     .getDisplayMedia({
+//       video: { cursor: 'always' },
+//       audio: { echoCancellation: true, noiseSuppression: true },
+//     })
+//     .then((stream) => {
+//       videoRef.current.srcObject = stream; // 내 비디오 공유 화면으로 변경
+//       const videoTrack = stream.getVideoTracks()[0];
+//       connectionRef.current
+//         .getSenders()
+//         .find((sender) => sender.track.kind === videoTrack.kind)
+//         .replaceTrack(videoTrack);
+//       videoTrack.onended = function () {
+//         const screenTrack = userStream.current.getVideoTracks()[0];
+//         connectionRef.current
+//           .getSenders()
+//           .find((sender) => sender.track.kind === screenTrack.kind)
+//           .replaceTrack(screenTrack);
+//         stream.getTracks().forEach((track) => track.stop());
+//         myVideo.current.srcObject = userStream.current; // 내 비디오로 변경
+//       };
+//     });
+// };
