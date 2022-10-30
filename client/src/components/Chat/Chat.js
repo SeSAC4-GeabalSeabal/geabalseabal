9e32899baf1e5a780d77ad52a4952c58a8483b74
mymj11 <pass8208@gmail.com>
2022-10-30 21:19:36 +0900

    docs:chat

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -5,16 +5,6 @@ import dayjs from "dayjs";
 import { renderMatches } from "react-router-dom";
 
   function Chat({socket, roomName}) {
-    // 내가 지금 1일 때. scss 편하게하는 법.
-    // let temp = [
-    //   {name: '김민지', message: '반갑습니다. 반가워요. 화이팅'},
-    //   {name: '김예은', message: '222.'},
-    //   {name: '곽시하', message: '123123.'},
-    //   {name: '최상훈', message: '11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111'},
-    //   {name: '1', message: '123312.'},
-    //   {name: '개발새발', message: '123312.'},
-    // ]
-    // const [chatArr, setChatArr] = useState(temp);
 
     const [chatArr, setChatArr] = useState([]);
     const inputRef = useRef(null); // 채팅 내용
@@ -54,21 +44,6 @@ import { renderMatches } from "react-router-dom";
       await socket.emit("new_message", message, roomName);
     }
 
-    //새 메시지 입력시 채팅 스크롤이 밑으로 이동해야 하는데 전체 화면 스크롤이 밑으로 내려감...
-    // const scrollToBottom = () => {
-    //   inputRef.current?.scrollIntoView({behavior:"smooth"});
-    // };
-    // useEffect(() => {
-    //   scrollToBottom();
-    // },[chatArr]);
-
-    // const handleClick = () => {
-    //   inputRef.current.scrollIntoView({behavior:'smooth'});
-    // };
-    // useEffect(()=>{
-    //   handleClick();
-    // },[chatArr]);
-
     return (
       <div className="ChatApp">
         <div className="Box">
@@ -96,17 +71,5 @@ import { renderMatches } from "react-router-dom";
 
   export default Chat;
 
-
-
-
-
-  // const scrollToBottom = () => {
-  //   if(inputRef.current){
-  //     inputRef.current.scrollTop = inputRef.current.scrollHeight;
-  //   }
-  // };
-  // useEffect(() => {
-  //   scrollToBottom();
-  // },[chatArr]);
 
 
