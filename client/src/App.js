ef13cda59bec69b3c7ab6d5f151d663c0eebba87
yeaeunkim <mangokim96@gmail.com>
2022-10-31 01:40:01 +0900

    feat: 신택스하이라이트  진행중. 코드블럭에 색은 입혀지는데 언어매칭 해야함..

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,11 @@
-
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import { Nav, Login } from './components';
-import Room from './components/Room/Room';
-import Home from './components/home/Home';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Nav, Login } from "./components";
+import Room from "./components/Room/Room";
+import Home from "./components/home/Home";
 // import Chat from './components/Chat/Chat';
-import Footer from './components/footer/Footer';
-import axios from 'axios';
-import {useEffect, useState} from 'react';
+import Footer from "./components/footer/Footer";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const App = () => {
   const [isLogin, setIsLogin] = useState(false);
@@ -16,12 +15,11 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home setIsLogin={setIsLogin} isLogin={isLogin} />}></Route>
         <Route path="/login" element={<Login />}></Route>
-        <Route path='/Room' element={<Room />}></Route>
+        <Route path="/Room" element={<Room />}></Route>
         {/* <Route path='/chat' element={<Chat />}></Route> */}
       </Routes>
       <Footer />
     </BrowserRouter>
   );
-}
+};
 export default App;
-
