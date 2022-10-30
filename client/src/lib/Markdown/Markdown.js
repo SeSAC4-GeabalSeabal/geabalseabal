8d1ebf92f620f91342527a6df9db2fb080632e5b
yeaeunkim <mangokim96@gmail.com>
2022-10-31 00:07:45 +0900

    feat: 코드수정..

--- client/src/lib/Markdown/Markdown.js
+++ client/src/lib/Markdown/Markdown.js
@@ -1,6 +1,8 @@
 import React, { useCallback } from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { dark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 const Markdown = React.memo(({ linkStopPropagation, ...props }) => {
   const handleLinkClick = useCallback((event) => {
